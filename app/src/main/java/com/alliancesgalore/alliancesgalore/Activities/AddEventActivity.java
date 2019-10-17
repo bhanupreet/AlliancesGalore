277e67e60cf68a70f61995feb6e5662a08dae3ba
bhanupreet <bhanupreet14@gmail.com>
2019-10-17 16:56:32 +0530

    added Event.java which will map the data from CalendarEvent.java for firebase.

--- app/src/main/java/com/alliancesgalore/alliancesgalore/Activities/AddEventActivity.java
+++ app/src/main/java/com/alliancesgalore/alliancesgalore/Activities/AddEventActivity.java
@@ -1,32 +1,74 @@
 package com.alliancesgalore.alliancesgalore.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.DatePicker;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alliancesgalore.alliancesgalore.R;
 import com.alliancesgalore.alliancesgalore.Utils.Functions;
-import com.google.android.material.textfield.TextInputEditText;
+import com.github.tibolte.agendacalendarview.models.BaseCalendarEvent;
+import com.github.tibolte.agendacalendarview.models.CalendarEvent;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
-public class AddEventActivity extends AppCompatActivity {
+public class AddEventActivity extends AppCompatActivity implements View.OnClickListener {
     private Toolbar mToolBar;
     private TextInputLayout mTitle;
-    private TextView mStartDate;
-    private Calendar calendar;
+    private TextView mStartDate, mEndDate, mStartTime, mEndTime, getEventText;
+    private Calendar cStartDate = Calendar.getInstance(), cEndDate = Calendar.getInstance();
     private int mYear, mMonth, mDay;
     private Context mCtx = AddEventActivity.this;
+    private Button getevent, setevent;
+    private List<CalendarEvent> eventList = new ArrayList<>();
+    private ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            if (dataSnapshot.exists()) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Event baseCalendarEvent = snapshot.getValue(Event.class);
+                    Calendar start = Calendar.getInstance();
+                    start.setTimeInMillis(baseCalendarEvent.getStartTime());
+                    Calendar end = Calendar.getInstance();
+                    end.setTimeInMillis(baseCalendarEvent.getEndTime());
+
+                    BaseCalendarEvent event1 = new BaseCalendarEvent(baseCalendarEvent.getTitle(),
+                            "A wonderful journey!",
+                            "Iceland",
+                            ContextCompat.getColor(AddEventActivity.this, R.color.colorPrimaryDark),
+                            start, end, baseCalendarEvent.isAllDay());
+                    eventList.add(event1);
+                }
+                getEventText.setText(eventList.get(0).getTitle());
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,24 +77,10 @@ public class AddEventActivity extends AppCompatActivity {
         FindIds();
         SetmToolBar();
 
-        mStartDate.setOnClickListener(view -> {
-            calendar = Calendar.getInstance();
-            mYear = calendar.get(Calendar.YEAR);
-            mMonth = calendar.get(Calendar.MONTH);
-            mDay = calendar.get(Calendar.DAY_OF_MONTH);
-
-
-            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                    (view1, year, monthOfYear, dayOfMonth) -> {
-                        mStartDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
-                        calendar.set(year, monthOfYear, dayOfMonth);
-                        Date date = calendar.getTime();
-                        String full = new SimpleDateFormat("dd-MM-yyyy").format(date);
-                        Functions.toast(full, mCtx);
-                    }, mYear, mMonth, mDay);
-
-            datePickerDialog.show();
-        });
+        mStartDate.setOnClickListener(this);
+        mEndDate.setOnClickListener(this);
+        getevent.setOnClickListener(this);
+        setevent.setOnClickListener(this);
 
     }
 
@@ -61,12 +89,16 @@ public class AddEventActivity extends AppCompatActivity {
         mToolBar = findViewById(R.id.addEvent_toolbar);
         mStartDate = findViewById(R.id.addEvent_startDate);
         mTitle = findViewById(R.id.addEvent_title);
+        mEndDate = findViewById(R.id.addEvent_endDate);
+        getevent = findViewById(R.id.geteventbtn);
+        setevent = findViewById(R.id.setEventbtn);
+        getEventText = findViewById(R.id.getEventText);
     }
 
     private void SetmToolBar() {
         setSupportActionBar(mToolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Add Title");
+        getSupportActionBar().setTitle("Add Event");
     }
 
     @Override
@@ -79,5 +111,64 @@ public class AddEventActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Initializes the event
+     *
+     * @param title       The title of the event.
+     * @param description The description of the event.
+     * @param location    The location of the event.
+     * @param color       The color of the event (for display in the app).
+     * @param startTime   The start time of the event.
+     * @param endTime     The end time of the event.
+     * @param allDay      Indicates if the event lasts the whole day.
+     */
+    @Override
+    public void onClick(View view) {
+
+        if (mStartDate == (view)) {
+            setDate(mStartDate);
+        }
+        if (view == mEndDate) {
+            setDate(mEndDate);
+        }
+        if (view == getevent) {
+            FirebaseDatabase.getInstance().getReference().child("CalendarEvents").addValueEventListener(valueEventListener);
+        }
+        if (view == setevent) {
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("CalendarEvents").push();
+            ref.getKey();
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("Title", Functions.TextOf(mTitle));
+            map.put("Description", "default");
+            map.put("Location", "default");
+            map.put("Color", 3);
+            map.put("StartTime", cStartDate.getTimeInMillis());
+            map.put("EndTime", cEndDate.getTimeInMillis());
+
+            map.put("AllDay", true);
+
+            ref.setValue(map).addOnSuccessListener(aVoid -> Log.d("data upload", "success"));
+        }
+
+    }
+
+    private void setDate(TextView dateview) {
+        Calendar calendar = Calendar.getInstance();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view1, year, monthOfYear, dayOfMonth) -> {
+                    dateview.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                    calendar.set(year, monthOfYear, dayOfMonth);
+                    Date date = calendar.getTime();
+                    String full = new SimpleDateFormat("dd-MM-yyyy").format(date);
+                    Functions.toast(full, mCtx);
+                }, mYear, mMonth, mDay);
+
+        datePickerDialog.show();
     }
 }
---
+++ app/src/main/java/com/alliancesgalore/alliancesgalore/Activities/Event.java
@@ -0,0 +1,91 @@
+package com.alliancesgalore.alliancesgalore.Activities;
+
+public class Event {
+    private int Color;
+    private boolean AllDay;
+    private long StartTime;
+    private long EndTime;
+    private String Location;
+    private String Title;
+    private String Description;
+
+    public Event() {
+    }
+
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public int getColor() {
+        return Color;
+    }
+
+    public void setColor(int color) {
+        Color = color;
+    }
+
+    public long getStartTime() {
+        return StartTime;
+    }
+
+    public void setStartTime(long startTime) {
+        StartTime = startTime;
+    }
+
+    public boolean isAllDay() {
+        return AllDay;
+    }
+
+    public void setAllDay(boolean allDay) {
+        AllDay = allDay;
+    }
+
+    public long getEndTime() {
+        return EndTime;
+    }
+
+    public void setEndTime(long endTime) {
+        EndTime = endTime;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    /**
+     * Initializes the event
+     *  @param title       The title of the event.
+     * @param description The description of the event.
+     * @param location    The location of the event.
+     * @param color       The color of the event (for display in the app).
+     * @param startTime   The start time of the event.
+     * @param endTime     The end time of the event.
+     * @param allDay      Indicates if the event lasts the whole day.
+     */
+    public Event(String title, String description, String location, int color, long startTime, long endTime, boolean allDay) {
+        this.Title = title;
+        this.Description = description;
+        this.Location = location;
+        this.Color = color;
+        this.StartTime = startTime;
+        this.EndTime = endTime;
+        this.AllDay = allDay;
+    }
+}
